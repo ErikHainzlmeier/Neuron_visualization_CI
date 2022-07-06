b76192fc7e1d96473226161c47eead0e5250bf9b
ErikHainzlmeier <erik.hainzlmeier@tum.de>
2022-07-06 14:29:44 +0200

    Update Neurons.py
    creation of curves following the neurons successful

--- Neurons.py
+++ Neurons.py
@@ -10,14 +10,26 @@ obj = pd.read_pickle(filepath)
 # ci_refine_list_mdl.pkl
 # coordinates for the neuron paths
 # list of 400 elements of variable length x 3 matrix
-fibreverts = []
-fibreedges = []
-fibrefaces = []
-for coor in obj[i][:]:
-    # Create vertices following neuron fibres coordinate
-    fibreverts.append(coor)
-for verindex in range(0, len(fibreverts) - 1):
-    fibreedges.append([verindex, verindex + 1])
 
-positionOrder = [cmds.pointPosition(i) for i in fibreverts]
-cmds.curve(p=positionOrder)
+vertices = om.MPointArray()
+for i in range(0,400,1):
+    vertices = om.MPointArray()
+    for eachPos in obj[i][:] :
+# make a point array out of coordinates from .pkl file for every neuron
+        mPoint      = om.MPoint ()
+        mPoint.x    = eachPos[0]
+        mPoint.y    = eachPos[1]
+        mPoint.z    = eachPos[2]
+        vertices.append (mPoint)
+
+        #print(len(obj[i][:]))
+    #if i != 0:
+        #cmds.curve(pw=vertices[len(obj[i-1][:])+1 : len(obj[i-1][:])+len(obj[i][:])])
+        #print(vertices[len(obj[i-1][:])+1 : len(obj[i-1][:])+len(obj[i][:])])
+    #else:
+
+# create a curve for every neuron following along the vertices
+    cmds.curve(pw=vertices)
+
+
+
