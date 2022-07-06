664d9c5b687d3dd315a4c8f1bed1b3f2e87fa88e
ErikHainzlmeier <erik.hainzlmeier@tum.de>
2022-07-06 09:00:58 +0200

    Update Neurons.py

--- Neurons.py
+++ Neurons.py
@@ -1,28 +1,23 @@
-
 import numpy as np
 import pandas as pd
-
+import maya.api.OpenMaya as om
 
 
 filepath = "C:\\Users\\Erik\\Documents\\Elektrotechnik\\Master\\SS2022\\Projektpraktikum\\ci_refine_list_mdl.pkl"
 obj = pd.read_pickle(filepath)
 
+#Create neuron fibres from coordinate data
+# ci_refine_list_mdl.pkl
+# coordinates for the neuron paths
+# list of 400 elements of variable length x 3 matrix
+fibreverts = []
+fibreedges = []
+fibrefaces = []
+for coor in obj[i][:]:
+    # Create vertices following neuron fibres coordinate
+    fibreverts.append(coor)
+for verindex in range(0, len(fibreverts) - 1):
+    fibreedges.append([verindex, verindex + 1])
 
-
-
-
-for i in range(0,399,2):
-    for j in range(len(obj[i])-2):
-        x_vals = [obj[i][j][0], obj[i][j+1][0]]
-        y_vals = [obj[i][j][1], obj[i][j+1][1]]
-        z_vals = [obj[i][j][2], obj[i][j+1][2]]
-
-
-plt.show()
-
-
-
-
-
-
-#result = cmds.polyCylinder( w=0.01, h=9, d=9, name='myCube#')
+positionOrder = [cmds.pointPosition(i) for i in fibreverts]
+cmds.curve(p=positionOrder)
