daa64af572a8da1298bb06c81171dcb580e13d7e
ErikHainzlmeier <erik.hainzlmeier@tum.de>
2022-07-24 18:22:12 +0200

    changing the csv file to pkl file
    changing the csv file to pkl file and making it to millimeters

---
+++ pickle_compartment_lengths.py
@@ -0,0 +1,20 @@
+import csv
+import pickle
+
+compartmentlengths_array = []
+with open('R13_2.3mm_compartment_lengths.csv', newline='') as inputfile:
+    # transforming the compartmentlengths from the csv file from meter to millimeter in order to make them the same
+    # unit as the coordinates for the neurons
+    for row in csv.reader(inputfile):
+        compartmentlengths_array.append(float(row[0])*1e3)
+
+with open('compartmentlengths_mm.pkl', 'wb') as f:
+    pickle.dump(compartmentlengths_array, f)
+
+with open('compartmentlengths_mm.pkl', 'rb') as f:
+    results = pickle.load(f)
+
+#results = np.array(results)
+
+
+print(results)
