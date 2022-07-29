fba8917eeaadd38e8fe0cc9df32c55e04073e3e3
ErikHainzlmeier <erik.hainzlmeier@tum.de>
2022-07-29 14:08:34 +0200

    plot_vertices

--- coordinates_compartments.py
+++ coordinates_compartments.py
@@ -16,7 +16,8 @@ def geometry(x_dist, y_dist, z_dist, distance, pos1, restcomp_len):
     geometry_point.append(pos1[1])
     geometry_point.append(pos1[2])
     rest_distance = distance - restcomp_len
-
+    cmds.polySphere(r=0.01)
+    cmds.move(geometry_point[0], geometry_point[1], geometry_point[2])
     return geometry_point, rest_distance
 
 def main():
@@ -24,13 +25,14 @@ def main():
     comp_coords=[]
     #with open('compartmentlengths_mm.pkl', 'rb') as compartment_lengths:
     compartment_lengths = pd.read_pickle(r'C:\\Users\\Erik\\Documents\\Elektrotechnik\\Master\\SS2022\\Projektpraktikum\\Neuron_visualization_CI\\compartmentlengths_mm.pkl')
-    k=0
+
     for i in range(0, len(vertices)-1): # from neuron 0 to 399
         comp_coords.append([])
-        for eachPos in range(0,len(vertices[i])):
-            x_dist = (vertices[i + 1][eachPos][0] - vertices[i][eachPos][0])
-            y_dist = (vertices[i + 1][eachPos][1] - vertices[i][eachPos][1])
-            z_dist = (vertices[i + 1][eachPos][2] - vertices[i][eachPos][2])
+        k = 0
+        for eachPos in range(0,len(vertices[i])-1):
+            x_dist = (vertices[i][eachPos+1][0] - vertices[i][eachPos][0])
+            y_dist = (vertices[1][eachPos+1][1] - vertices[i][eachPos][1])
+            z_dist = (vertices[i][eachPos+1][2] - vertices[i][eachPos][2])
             distance = math.sqrt(x_dist**2 + y_dist**2 + z_dist**2)
             if compartment_lengths[k] <= distance:
                 while compartment_lengths[k] <= distance:
@@ -38,7 +40,9 @@ def main():
                     comp_coords[i].append(geometry_point)
                     k += 1
                     distance = rest_distance
+
                 compartment_lengths[k] = compartment_lengths[k] - distance
+
             else:
                 compartment_lengths[k] = compartment_lengths[k] - distance
     print(len(compartment_lengths))
