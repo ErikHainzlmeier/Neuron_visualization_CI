b49189117fb44c6c92801375e9f6594f4981f63b
ErikHainzlmeier <erik.hainzlmeier@tum.de>
2022-08-22 14:10:43 +0200

    Delete camera.py
    deleted python script camera.py

--- camera.py
+++
@@ -1,6 +0,0 @@
-cameraName = cmds.camera()
-cameraShape = cameraName[1]
-aimLoc = 'mySphere18'
-cmds.aimConstraint(aimLoc, cameraName[0], aimVector=(0,0,-1))
-cmds.setAttr(cameraName[0]+".scaleY", -1)
-#cmds.viewPlace(cameraName[0] )
