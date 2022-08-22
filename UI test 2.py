2f912262a734fbb5256cde191d472afa117c35c1
F0rtas <rafael.vorlaender@gmail.com>
2022-08-22 11:00:17 +0200

    finished UI test 2

--- UI test 2.py
+++ UI test 2.py
@@ -66,7 +66,7 @@ class MR_Window(object):
         self.import_tube = cmds.checkBox(label=' ')
         cmds.setParent('..')
         cmds.rowColumnLayout(numberOfColumns=1, columnAttach=(1, 'left', 0), columnWidth=[(1, 395)])
-        self.cubeSubdivs = cmds.floatSliderGrp(field=True, label='Transparency:', minValue=0, maxValue=1, value=0.9, columnAlign=[1, 'right'])
+        self.tube_transparency = cmds.floatSliderGrp(field=True, label='Transparency:', minValue=0, maxValue=1, value=0.9, columnAlign=[1, 'right'])
         self.tube_color = cmds.colorSliderGrp(label='Colour:', rgb=(0.7, 0.7, 0.7),columnAlign=[1, 'right'])
 
         cmds.setParent('..')
@@ -76,7 +76,7 @@ class MR_Window(object):
         self.import_nerve = cmds.checkBox(label=' ')
         cmds.setParent('..')
         cmds.rowColumnLayout(numberOfColumns=1, columnAttach=(1, 'left', 0), columnWidth=[(1, 395)])
-        self.cubeSubdivs = cmds.floatSliderGrp(field=True, label='Transparency:', minValue=0, maxValue=1, value=0, columnAlign=[1, 'right'])
+        self.nerve_transparency = cmds.floatSliderGrp(field=True, label='Transparency:', minValue=0, maxValue=1, value=0, columnAlign=[1, 'right'])
         self.nerve_color = cmds.colorSliderGrp(label='Colour:', rgb=(0.5, 0.7, 1),columnAlign=[1, 'right'])
         cmds.text(" ")
         cmds.text(" ")
@@ -98,23 +98,47 @@ class MR_Window(object):
         #Build model button
         cmds.setParent('..')
         cmds.separator(height=30)
-        cmds.button(label='Build Model', bgc=[0.4, 0.65, 0.2], command=self.run_variables())
+        cmds.button(label='Build Model', bgc=[0.4, 0.65, 0.2], command=self.run_variables)
 
         # display new window
         cmds.showWindow()
 
     def run_variables(self, *args):
-        path = cmds.textFieldGrp(self.filepath, query=True, text=True)
+        path = cmds.textField(self.filepath, query=True, text=True)
+        measur_stepsize = cmds.intField(self.measur_stepsize, query=True, value=True)
+        firstNeur = cmds.intField(self.firstNeur, query=True, value=True)
+        lastNeur = cmds.intField(self.lastNeur, query=True, value=True)
+        neur_stepsize = cmds.intField(self.neur_stepsize, query=True, value=True)
+        import_cochlea = cmds.checkBox(self.import_cochlea, query=True, value=True)
+        cochlea_transparency = cmds.floatSliderGrp(self.cochlea_transparency, query=True, value=True)
+        cochlea_color = cmds.colorSliderGrp(self.cochlea_color, query=True, rgbValue=True)
+        import_tube = cmds.checkBox(self.import_tube, query=True, value=True)
+        tube_transparency = cmds.floatSliderGrp(self.tube_transparency, query=True, value=True)
+        tube_color = cmds.colorSliderGrp(self.tube_color, query=True, rgbValue=True)
+        import_nerve = cmds.checkBox(self.import_nerve, query=True, value=True)
+        nerve_transparency = cmds.floatSliderGrp(self.nerve_transparency, query=True, value=True)
+        nerve_color = cmds.colorSliderGrp(self.nerve_color, query=True, rgbValue=True)
+        light_intensity = cmds.floatSliderGrp(self.light_intensity, query=True, value=True)
+        camera_speed = cmds.floatSliderGrp(self.camera_speed, query=True, value=True)
 
-        measurement_stepsize = cmds.intField(self.measur_stepsize, query=True)
-
-        width = cmds.floatFieldGrp(self.firstNeur, query=True, value1=True)
-        height = cmds.floatFieldGrp(self.lastNeur, query=True, value2=True)
-        depth = cmds.floatFieldGrp(self.stepsize, query=True, value3=True)
-
-        subdivs = cmds.intSliderGrp(self.cubeSubdivs, query=True, value=True)
 
         print("path:", path)
-        print("measurement stepsize:", measurement_stepsize)
+        print("measurement stepsize:", measur_stepsize)
+        print("firstNeur:", firstNeur)
+        print("lastNeur:", lastNeur)
+        print("neur_stepsize:", neur_stepsize)
+        print("import_cochlea:", import_cochlea)
+        print("cochlea_transparency:", cochlea_transparency)
+        print("cochlea_color:", cochlea_color)
+        print("import_tube:", import_tube)
+        print("tube_transparency:", tube_transparency)
+        print("tube_color:", tube_color)
+        print("import_nerve:", import_nerve)
+        print("nerve_transparency:", nerve_transparency)
+        print("nerve_color:", nerve_color)
+        print("light_intensity:", light_intensity)
+        print("camera_speed:", camera_speed)
+
+        cmds.deleteUI(self.window, window=True)
 
 myWindow = MR_Window()
